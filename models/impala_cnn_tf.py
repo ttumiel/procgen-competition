af3739ed3c57de5b999cbc0ed12cba2aa05d192c
Thomas Tumiel <ttumiel@gmail.com>
2020-10-27 16:17:26 +0200

    change cnn to class based

--- models/impala_cnn_tf.py
+++ models/impala_cnn_tf.py
@@ -2,32 +2,73 @@ from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.rllib.utils.framework import try_import_tf
 from ray.rllib.models import ModelCatalog
 
-tf = try_import_tf()
+tf1,tf,tfv = try_import_tf()
 
 
 def conv_layer(depth, name, st=1):
     return tf.keras.layers.Conv2D(
-        filters=depth, kernel_size=3, strides=st, padding="same", name=name
+        filters=depth, kernel_size=3, strides=st, padding="same", name=name,
+        # kernel_initializer=tf.keras.initializers.he_normal()
     )
 
 
-def residual_block(x, depth, prefix):
-    inputs = x
-    assert inputs.get_shape()[-1].value == depth
-    x = tf.keras.layers.ReLU()(x)
-    x = conv_layer(depth, name=prefix + "_conv0")(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = conv_layer(depth, name=prefix + "_conv1")(x)
-    return x + inputs
+class ResBlock(tf.keras.layers.Layer):
+    def __init__(self, depth, prefix):
+        super().__init__(name=prefix+"_resblock")
+        self.d,self.p = depth,prefix
+    def build(self, shape):
+        assert shape[-1] == self.d
+        self.conv = tf.keras.Sequential([
+            tf.keras.layers.ReLU(),
+            conv_layer(self.d, name=self.p + "_conv0"),
+            tf.keras.layers.ReLU(),
+            conv_layer(self.d, name=self.p + "_conv1")
+        ])
+    def call(self,x):
+        return x + self.conv(x)
+
+class ConvSequence(tf.keras.Sequential):
+    def __init__(self, depth, prefix, st=1):
+        super(ConvSequence, self).__init__([
+            conv_layer(depth, prefix + "_conv", st=st),
+            tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding="same"),
+            ResBlock(depth, prefix+"_block0"),
+            ResBlock(depth, prefix+"_block1")
+        ])
 
 
-def conv_sequence(x, depth, prefix, st=1):
-    x = conv_layer(depth, prefix + "_conv", st=st)(x)
-    x = tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding="same")(x)
-    x = residual_block(x, depth, prefix=prefix + "_block0")
-    x = residual_block(x, depth, prefix=prefix + "_block1")
-    return x
+class ImpalaBase(tf.keras.Model):
+    def __init__(self, multiplier, hidden_act='relu'):
+        super().__init__(name="body")
+        self.m=multiplier
 
+        self.features = tf.keras.Sequential([
+            ConvSequence(channels*self.m, f"seq{i}", st=stride)
+            for i,(channels,stride) in enumerate([[16,1],[32,1],[64,1]])
+        ])
+
+        self.head = tf.keras.Sequential([
+            tf.keras.layers.GlobalAveragePooling2D(),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.Dense(units=256, activation=hidden_act, name="hidden")
+        ])
+
+    def call(self, inputs, training=None):
+        x = (tf.cast(inputs, tf.float32) - 128.0) / 255.0
+        x = self.features(x)
+        return self.head(x)
+
+class Augment(tf.keras.layers.Layer):
+    # Make sure latest tf is installed.
+    def __init__(self):
+        super().__init__()
+        self.augs = tf.keras.Sequential([
+            tf.keras.layers.experimental.preprocessing.RandomTranslation(0.15, 0.15, fill_mode='reflect'),
+            tf.keras.layers.experimental.preprocessing.RandomZoom(0.1, fill_mode='reflect')
+        ])
+    def call(self, x, training=None):
+        return self.augs(x, training=training)
 
 class ImpalaCNN(TFModelV2):
     """
@@ -37,44 +78,47 @@ class ImpalaCNN(TFModelV2):
     and https://github.com/openai/baselines/blob/9ee399f5b20cd70ac0a871927a6cf043b478193f/baselines/common/models.py#L28
     """
 
-    def __init__(self, obs_space, action_space, num_outputs, model_config, name, **kwargs):
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
         super().__init__(obs_space, action_space, num_outputs, model_config, name)
 
-        args = model_config['custom_options']
+        args = model_config['custom_model_config']
+        self.a,self.f = args['augment'],args['framestack']
 
-        self._framestack = args['framestack']
+        # self._framestack = args['framestack']
+        obs_shape = obs_space.shape
         if args['framestack']:
-            s,h,w,c=obs_space.shape
+            s,h,w,c = obs_space.shape
             obs_shape = (h,w,c*s)
-        else:
-            obs_shape=obs_space.shape
 
-        # obs_space.shape
+        if self.f:
+            self.frames = tf.keras.Sequential([
+                tf.keras.layers.Permute((2,3,1,4)),
+                tf.keras.layers.Reshape(obs_shape)
+            ])
+
+        body = ImpalaBase(1)
         inputs = tf.keras.layers.Input(shape=obs_space.shape, name="observations")
-        scaled_inputs = (tf.cast(inputs, tf.float32) - 128.0) / 255.0
-        x = scaled_inputs
-
-        if self._framestack:
-            # Framestack
-            x = tf.keras.layers.Permute((2,3,1,4))(x)
-            x = tf.keras.layers.Reshape(obs_shape)(x)
-
-        for i, (channels, stride) in enumerate(model_config['conv_filters']):
-            x = conv_sequence(x, channels, prefix=f"seq{i}", st=stride)
-
-        x = tf.keras.layers.Flatten()(x)
-        x = tf.keras.layers.ReLU()(x)
-        x = tf.keras.layers.Dense(units=256, activation="relu", name="hidden")(x)
-        logits = tf.keras.layers.Dense(units=num_outputs, name="pi")(x)
-        value = tf.keras.layers.Dense(units=1, name="vf")(x)
+        x = body(inputs)
+        logits = tf.keras.layers.Dense(units=num_outputs, name="pi",)(x)
+                    # kernel_initializer=tf.keras.initializers.he_normal())(x)
+        value = tf.keras.layers.Dense(units=1, name="vf",)(x)
+                    # kernel_initializer=tf.keras.initializers.he_normal())(x)
         self.base_model = tf.keras.Model(inputs, [logits, value])
         self.register_variables(self.base_model.variables)
+        self.base_model.summary()
 
     def forward(self, input_dict, state, seq_lens):
-        # explicit cast to float32 needed in eager
-        obs = tf.cast(input_dict["obs"], tf.float32)
+        obs = input_dict["obs"]
 
-        logits, self._value = self.base_model(obs)
+        # Transform Framestack
+        if self.f:
+            obs = self.frames(obs)
+
+        # Augment inputs
+        if self.a:
+            obs = Augment()(obs, training=input_dict['is_training'])
+
+        logits, self._value = self.base_model(obs, training=input_dict['is_training'])
         return logits, state
 
     def value_function(self):
