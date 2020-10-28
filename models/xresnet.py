c4bbe5487d1c35c555ec8f8da4159f1c84599774
Thomas Tumiel <ttumiel@gmail.com>
2020-10-28 22:20:39 +0200

    add self attention and SE blocks

--- models/xresnet.py
+++ models/xresnet.py
@@ -2,30 +2,79 @@ from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.rllib.utils.framework import try_import_tf
 from ray.rllib.models import ModelCatalog
 # import tensorflow_addons as tfa
+import math
 
 tf1,tf,tfv = try_import_tf()
 
 
 def conv(c, k=3, st=1, act=tf.keras.layers.ReLU, name="", gamma_init='ones'):
     return tf.keras.Sequential([
-                tf.keras.layers.Conv2D(c, k, strides=st, padding='same', use_bias=False),
+                tf.keras.layers.Conv2D(c, k, strides=st, padding='same', use_bias=True),
                 # use_bias=False, kernel_initializer=tf.keras.initializers.HeNormal()),
             # tf.keras.layers.BatchNormalization(gamma_initializer=gamma_init),
             # tfa.layers.GroupNormalization(gamma_initializer=gamma_init),
     ]+([act()] if act is not None else []))
 
 
+class SelfAttention(tf.keras.layers.Layer):
+    def __init__(self, channels):
+        super().__init__(name="SelfAttention")
+        self.channels = channels
+        self.gamma = tf.Variable(0., name='gamma')
+
+    def _conv(self, cout):
+        # tfa.layers.SpectralNormalization()
+        return tf.keras.layers.Conv1D(cout, 1)
+
+    def build(self, shape):
+        self.q = self._conv(self.channels // 8)
+        self.k = self._conv(self.channels // 8)
+        self.v = self._conv(self.channels)
+        self.flatten = tf.keras.layers.Reshape((-1, shape[-1]))
+        self.unflatten = tf.keras.layers.Reshape(shape[1:])
+
+    def call(self, x):
+        x = self.flatten(x)
+        f,g,h = self.q(x),self.k(x),self.v(x)
+        beta = tf.nn.softmax(tf.matmul(g, f, transpose_b=True))
+        o = self.gamma * tf.matmul(beta, h) + x
+        return self.unflatten(o)
+
+class Sigmoid(tf.keras.layers.Layer):
+    def call(self, x):
+        return tf.keras.activations.sigmoid(x)
+
+class SEBlock(tf.keras.layers.Layer):
+    def __init__(self, channels, reduction):
+        super().__init__(name="SEBlock")
+        self.nf = math.ceil(channels//reduction/8)*8
+
+    def build(self, shape):
+        self.squeeze_excite = tf.keras.Sequential([
+            tf.keras.layers.AveragePooling2D(shape[1:3]),
+            conv(self.nf, 1),
+            conv(shape[-1], 1, act=Sigmoid),
+        ])
+
+    def call(self, x):
+        return x*self.squeeze_excite(x)
+
+
 class XResBlock(tf.keras.layers.Layer):
-    def __init__(self, channels, st=1, ks=3, act=tf.keras.layers.ReLU, prefix=""):
+    def __init__(self, channels, st=1, ks=3, reduction=1, self_att=False, act=tf.keras.layers.ReLU, prefix=""):
         super().__init__(name=prefix+"_xresblock")
-        self.d,self.st,self.ks,self.act = channels,st,ks,act
+        self.d,self.st,self.ks,self.act,self.red,self.sa = channels,st,ks,act,reduction,self_att
 
     def build(self, shape):
         self.cin = shape[-1]
-        self.conv = tf.keras.Sequential([
+        convs = [
             conv(self.d, st=self.st, act=self.act, name="_conv0"),
             conv(self.d, act=None, name="_conv1", gamma_init='zeros')
-        ])
+        ]
+        if self.red>1: convs.append(SEBlock(self.d, self.red))
+        if self.sa: convs.append(SelfAttention(self.d))
+        self.conv = tf.keras.Sequential(convs)
+
         short = []
         if self.st>1: short.append(tf.keras.layers.AveragePooling2D(self.st, self.st))
         if self.cin!=self.d: short.append(conv(self.d, k=1, act=None))
@@ -39,8 +88,8 @@ class XResNetBase(tf.keras.Model):
     def __init__(self, hidden_act='relu'):
         super().__init__()
         self.features = tf.keras.Sequential([
-            XResBlock(channels, prefix=f"seq{i}", st=stride)
-            for i,(channels,stride) in enumerate([[16,2],[32,2],[64,2],[128,2]])
+            XResBlock(channels, prefix=f"seq{i}", st=stride, reduction=red, self_att=sa)
+            for i,(channels,stride,sa,red) in enumerate([[32,2,False,1],[32,2,False,1],[64,2,False,1],[64,1,False,1],[64,1,False,1],[64,1,False,1],[64,1,False,1]])
         ])
 
         self.head = tf.keras.Sequential([
